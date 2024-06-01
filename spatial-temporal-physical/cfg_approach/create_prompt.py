bb25cbee0d6c928358000460d30d0fc68aa8670e
Jonathan Carlson <jonathanbcarlson@gmail.com>
2024-06-01 12:28:38 -0700

    use 4 choices for the 15 transport based spatial, temporal, physical prompts

--- spatial-temporal-physical/cfg_approach/create_prompt.py
+++ spatial-temporal-physical/cfg_approach/create_prompt.py
@@ -8,26 +8,54 @@ def create_prompt(
     transport_method_1: str,
     transport_method_2: str,
     transport_method_3: str,
+    transport_method_4: str,
+    transport_method_5: str,
     days_layer: int,
     destination: str,
     distance: int,
     duration_1: str,
     duration_2: str,
+    duration_3: str,
+    duration_4: str,
 ):
     prompt = f"""If I {transport_method_1} into the street and {days_layer} days later
     I find myself by the {destination} which is {distance} miles away.
     How could I have gotten to the {destination}?
     1. I {transport_method_2} for {duration_1}.
     2. I {transport_method_3} for {duration_2}.
-    Please choose the most likely answer and only respond with the number 1 or 2.
+    3. I {transport_method_4} for {duration_3}.
+    4. I {transport_method_5} for {duration_4}.
+    Please choose the most likely answer and only respond with a single number 1, 2, 3, or 4.
     """
     prompt = dedent(prompt)
     return prompt
 
 
-transport_methods = ["bike", "run", "sprint", "jog", "swim", "walk", "ride a horse"]
-durations = ["a quarter of a day", "10 hours", "3 hours", "half a day", "1 hour"]
-destinations = ["ocean", "mountains", "plains", "desert"]
+transport_methods = [
+    "bike",
+    "run",
+    "sprint",
+    "jog",
+    "swim",
+    "walk",
+    "ride a horse",
+    "hike",
+    "march",
+    "trek",
+    "tiptoe",
+]
+durations = [
+    "a quarter of a day",
+    "10 hours",
+    "3 hours",
+    "half a day",
+    "1 hour",
+    "half an hour",
+    "2 days",
+    "1 minute",
+    "5 minutes",
+]
+destinations = ["ocean", "mountains", "plains", "desert", "hills", "savannah"]
 days_later = list(range(100))
 distances = list(range(100))
 PROMPTS_JSON_FILENAME = "transport_spatial_temporal_physical_prompts.json"
@@ -36,23 +64,29 @@ PROMPTS_JSON_FILENAME = "transport_spatial_temporal_physical_prompts.json"
 def human_annotate_prompts(num_prompts_to_create):
     annotated_results = []
     while len(annotated_results) < num_prompts_to_create:
-        transport_choices = random.sample(transport_methods, 3)
-        duration_choices = random.sample(durations, 2)
+        transport_choices = random.sample(transport_methods, 5)
+        duration_choices = random.sample(durations, 4)
         created_prompt = create_prompt(
             transport_method_1=transport_choices[0],
             transport_method_2=transport_choices[1],
             transport_method_3=transport_choices[2],
+            transport_method_4=transport_choices[3],
+            transport_method_5=transport_choices[4],
             days_layer=random.sample(days_later, 1)[0],
             destination=random.sample(destinations, 1)[0],
             distance=random.sample(distances, 1)[0],
             duration_1=duration_choices[0],
             duration_2=duration_choices[1],
+            duration_3=duration_choices[2],
+            duration_4=duration_choices[3],
         )
         print(created_prompt)
         human_annotation = int(
-            input("enter human annotation (1 or 2) or 3 if ambiguous then hit enter\n")
+            input(
+                "enter human annotation (1, 2, 3, 4) or 5 if ambiguous then hit enter\n"
+            )
         )
-        if human_annotation == 3:
+        if human_annotation == 5:
             continue
         annotated_results.append(
             {"prompt": created_prompt, "human_annotation": human_annotation}
@@ -75,5 +109,5 @@ def llm_prompt_response(model_name: str):
 
 
 if __name__ == "__main__":
-    # human_annotate_prompts(10)
-    llm_prompt_response("gpt4o_llm_response")
+    human_annotate_prompts(15)
+    # llm_prompt_response("gpt4o_llm_response")
