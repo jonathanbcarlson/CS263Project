bcba9981ae23ad63c2dd013fda909bd5b5453ee1
Jonathan Carlson <jonathanbcarlson@gmail.com>
2024-06-01 18:47:19 -0700

    add physical-temporal prompts

---
+++ physical-temporal/generate_prompts.py
@@ -0,0 +1,29 @@
+import json
+
+instruction = "Please choose the most likely answer and only respond with a single number 1, 2, 3, or 4."
+
+
+def generate_prompts():
+    prompts = []
+    with open("backgrounds.txt") as backgrounds_f:
+        with open("options.txt") as options_f:
+            with open("human_annotations.txt") as human_annotations_f:
+                backgrounds = backgrounds_f.readlines()
+                options = options_f.read().split("|")
+                annotations = human_annotations_f.readlines()
+                for i in range(len(backgrounds)):
+                    background = backgrounds[i].strip()
+                    options_block = options[i].strip()
+                    annotation = int(annotations[i])
+                    prompts.append(
+                        {
+                            "prompt": f"{background}\n{options_block}\n{instruction}",
+                            "human_annotation": annotation,
+                        }
+                    )
+    with open("physical-temporal.json", "w", encoding="utf-8") as output_file:
+        json.dump(prompts, output_file, indent=2, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    generate_prompts()
